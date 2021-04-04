f74777458384fa7cfc9d94ef0d393393a8faf9e3
Jonah Leggett <jonah.leggett@gmail.com>
2021-04-03 19:50:04 -0700

    Part 4 Complete

--- Starter_Code/loan_analyzer.py
+++ Starter_Code/loan_analyzer.py
@@ -131,39 +131,42 @@ In this section, you will use a loop to iterate through a series of loans and se
 
 loans = [
     {
-        "loan_price": 700,
-        "remaining_months": 9,
+        'loan_price': 700,
+        'remaining_months': 9,
         "repayment_interval": "monthly",
-        "future_value": 1000,
+        'future_value': 1000,
     },
     {
-        "loan_price": 500,
-        "remaining_months": 13,
+        'loan_price': 500,
+        'remaining_months': 13,
         "repayment_interval": "bullet",
-        "future_value": 1000,
+        'future_value': 1000,
     },
     {
-        "loan_price": 200,
-        "remaining_months": 16,
+        'loan_price': 200,
+        'remaining_months': 16,
         "repayment_interval": "bullet",
-        "future_value": 1000,
+        'future_value': 1000,
     },
     {
-        "loan_price": 900,
-        "remaining_months": 16,
+        'loan_price': 900,
+        'remaining_months': 16,
         "repayment_interval": "bullet",
-        "future_value": 1000,
+        'future_value': 1000,
     },
 ]
 
 # @TODO: Create an empty list called `inexpensive_loans`
-# YOUR CODE HERE!
+inexpensive_loans = []
 
 # @TODO: Loop through all the loans and append any that cost $500 or less to the `inexpensive_loans` list
-# YOUR CODE HERE!
+for loan in loans:
+    loan_price = loan['loan_price']
+    if loan_price <= 500:
+        inexpensive_loans.append(loan_price)
 
 # @TODO: Print the `inexpensive_loans` list
-# YOUR CODE HERE!
+print(inexpensive_loans)
 
 
 """Part 5: Save the results.
